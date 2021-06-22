1793a04aedf00a77405ba2f0f0f6c2cf12355ac8
richardlara35 <richard.u.lara35@gmail.com>
2021-06-22 09:44:50 -0500

    ecercise

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+
+      int x = Integer.MIN_VALUE;
+
+        System.out.println(--x);
+
+    }
+
+}
