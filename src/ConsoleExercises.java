b2ac5a3b2ab69fec32bb5b83671aab9e1a8d8d3e
richardlara35 <richard.u.lara35@gmail.com>
2021-06-23 09:35:46 -0500

    morning

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 //        double pi = 3.14159;
-//        System.out.format("The value of pi is %f%n", pi );
+//        System.out.format("The value of pi is %.2f%n", pi );
         Scanner scanner = new Scanner(System.in);
 
 //        System.out.print("Enter a number: ");
@@ -17,11 +17,17 @@ public class ConsoleExercises {
 //        System.out.printf("Baby's first word: %s\n", wordOne);
 //        System.out.printf("Baby's second word: %s\n", wordTwo);
 //        System.out.printf("Baby's third word: %s\n", wordThree);
-
+//        scanner.useDelimiter(".");
+//        System.out.println("Please enter a sentence: ");
+//
+//        String userSentence = scanner.nextLine();
+//        System.out.println(userSentence);
         System.out.print("Lemme catch two numbers amigo: ");
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        System.out.println("%s\n * %s\n", num1, num2);
+        float length = Float.parseFloat(scanner.nextLine());
+        float width = Float.parseFloat(scanner.nextLine());
+
+        System.out.printf("Perimeter: %s\n", (2 * length) + (2 * width));
+        System.out.printf("Area: %s", length * width);
 
     }
 }
