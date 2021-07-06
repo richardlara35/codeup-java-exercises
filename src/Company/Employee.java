776988e79e8da3ccc43b9e0bf71c1222b4204c15
richardlara35 <richard.u.lara35@gmail.com>
2021-07-06 11:18:28 -0500

    lecture

---
+++ src/Company/Employee.java
@@ -0,0 +1,26 @@
+package Company;
+
+//abstract classes cannot ne instantiated
+//insantiation - to create an instance(an object) of that class
+//create a subclass from the abstract and then instantiate the subclass
+abstract class Employee {
+    //define properties
+    private String salary;
+    private String nameOfEmployee;
+    private String department;
+    private String title;
+
+    //if you don't define method in abstract class, you must mark it
+   public abstract String doWork(int hourlyRate);
+
+    public String doWork(){
+        return "I am working";
+    };
+
+    public static void main(String[] args) {
+        Manager bob = new Manager();
+        System.out.println(bob.doWork());
+    }
+}
+
+
