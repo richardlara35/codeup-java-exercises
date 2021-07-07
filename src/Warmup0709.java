82e59ee67a4f699389a1a46bee001a1cbc9abaca
richardlara35 <richard.u.lara35@gmail.com>
2021-07-07 11:57:27 -0500

    warmup

---
+++ src/Warmup0709.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Warmup0709 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Drop a String");
+        String userInput = sc.nextLine();
+
+        int count = 0;
+
+        for (int x = 0; x < userInput.length(); x++){
+            if (Character.isUpperCase(userInput.charAt(x))) count++;
+        }
+        System.out.println("There are " + count + " capital letters in this string");
+    }
+}
---
+++ src/abstractExercise/fruit.java
@@ -0,0 +1,11 @@
+package abstractExercise;
+
+//public class fruit extends fruits{
+//    public static void main(String[] args) {
+//        Fruit Mango = new Fruit();
+//        Mango.type = "No se";
+//        Mango.size = "small";
+//        Mango.seeds = "True";
+//        Mango.taste = "good";
+//    }
+//}
