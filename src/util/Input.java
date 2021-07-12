daeaafde3835a83c6ea51b9f25c31b09bec86023
richardlara35 <richard.u.lara35@gmail.com>
2021-07-12 16:15:16 -0500

    oop

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,60 @@
 package util;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Input {
+    private Scanner scanner;
+    private int num;
+    private double numDbl;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+        this.num = num;
+        this.numDbl = numDbl;
+    }
+
+    public int getInt() {
+        System.out.println("Enter an Integer");
+        return this.scanner.nextInt();
+    }
+
+    public int getInt(int min, int max) {
+        int number;
+        do {
+            System.out.printf("Enter a number b/w %d amd %d%n", min, max);
+            number = scanner.nextInt();
+        } while (number < min || number > max);
+        return number;
+    }
+
+    public String getString() {
+        String hello = "Hello";
+        return hello;
+    }
+
+    public boolean yesNo() {
+        System.out.println("Y/N? ");
+        String userChoice = scanner.next().toLowerCase();
+        if (userChoice.equals("y")) {
+            return true;
+        }
+        return false;
+    }
+    public double getDouble(double min, double max){
+        this.numDbl = getDouble();
+        if (numDbl < min){
+            System.out.println("Pick a number b/w " + min + " and " + max);
+            getDouble(min, max);
+        }else if (numDbl > max){
+            System.out.println("Pick a number b/w " + min + " and " + max);
+            getDouble(min, max);
+        }
+        return this.numDbl;
+    }
+
+    public double getDouble(){
+        System.out.println("Pick a number: ");
+        return scanner.nextDouble();
+    }
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,14 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("getInt: " + input.getInt());
+        System.out.println("yesNo: " + input.yesNo());
+        System.out.println("getDouble: " + input.getDouble());
+        System.out.println("getInt: " + input.getInt());
+        System.out.println("getDouble2: " + input.getDouble());
+        System.out.println("getString: " + input.getString());
+        System.out.println("getInt2: " + input.getInt());
+    }
+}
